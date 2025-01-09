f5b04b150a93ea3a4de6992d9be600126846f331
Camobeast <jakub.lilley@gmail.com>
2025-01-09 16:48:03 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure the build directory is set correctly
+    assetsDir: "assets", // Avoid issues with asset serving
+  },
   base: "/",
 });
