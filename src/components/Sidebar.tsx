16b56da032555fca835e9c9e3ac37f00ada444dd
Camobeast <jakub.lilley@gmail.com>
2025-01-10 22:31:35 +0100

    update

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -53,7 +53,7 @@ const Sidebar = ({
 
       {/* Sidebar */}
       <aside
-        className={`h-screen bg-base-300 p-4 fixed z-50 transition-transform duration-300 transform ${
+        className={`h-screen flex flex-col bg-base-300 p-4 fixed z-50 transition-transform duration-300 transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } lg:translate-x-0 lg:static`}
       >
