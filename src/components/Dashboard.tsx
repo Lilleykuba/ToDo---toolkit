72efa788f3b7da5a925bb65fba6d83804db3ffc2
Camobeast <jakub.lilley@gmail.com>
2025-02-25 22:19:03 +0100

    fix

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-const Dashboard = () => {
+const Dashboard = ({ onClose }: { onClose: () => void }) => {
   return (
     <div className="text-4xl text-center text-primary w-full mt-4">
       Dashboard
