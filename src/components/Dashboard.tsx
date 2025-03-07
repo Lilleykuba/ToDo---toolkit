97fde04cb8baa6d0b897da8b9a35288a575eb273
Camobeast <jakub.lilley@gmail.com>
2025-03-07 14:58:05 +0100

    fix

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -173,7 +173,7 @@ const Dashboard = ({
   };
 
   return (
-    <div className="flex flex-col w-full mt-4 justify-center items-start">
+    <div className="flex flex-col w-full mt-4 justify-start items-center">
       <div>
         <h2 className="text-primary text-3xl mb-3">
           Completed Tasks: {tasks.length}
