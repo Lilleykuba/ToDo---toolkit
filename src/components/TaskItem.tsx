f5317457645c07f73252f545bcfe61617fd2aca3
Camobeast <jakub.lilley@gmail.com>
2025-03-20 15:41:12 +0100

    addition of x button

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -72,7 +72,12 @@ const TaskItem = ({
     <>
       <dialog id="editTaskModal" className="modal">
         <div className="modal-box p-0">
-          <form method="dialog"></form>
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
           <EditTask taskId={task.id} />
         </div>
         <form method="dialog" className="modal-backdrop">
