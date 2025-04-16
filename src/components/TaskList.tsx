769c5ef0f056e4e3dadddb48fb2224aacb8e97fe
Camobeast <jakub.lilley@gmail.com>
2025-04-16 20:03:16 +0200

    minor improvement

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -223,7 +223,7 @@ const TaskList = ({
             <div
               {...provided.droppableProps}
               ref={provided.innerRef}
-              className="space-y-4 overflow-auto max-h-96"
+              className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 overflow-auto max-h-96"
             >
               {tasks.map((task, index) => (
                 <Draggable key={task.id} draggableId={task.id} index={index}>
